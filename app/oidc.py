49058456be4af5f967bd75ae25ba9698eefcf997
micafer <micafer1@upv.es>
2023-07-20 12:07:32 +0200

    Enable to set redirect_uri in OIDC

--- app/oidc.py
+++ app/oidc.py
@@ -19,10 +19,18 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import json
 import flask
 import logging
 from flask_dance.consumer import OAuth2ConsumerBlueprint, oauth_before_login
-from flask import url_for, redirect
+from flask import request, url_for, redirect, current_app
+from werkzeug.wrappers import Response
+from oauthlib.oauth2 import MissingCodeError
+from flask_dance.consumer.base import (
+    oauth_authorized,
+    oauth_before_login,
+    oauth_error,
+)
 
 log = logging.getLogger(__name__)
 
@@ -80,3 +88,83 @@ class OAuth2WithURIConsumerBlueprint(OAuth2ConsumerBlueprint):
         log.debug("redirect URL = %s", url)
         oauth_before_login.send(self, url=url)
         return redirect(url)
+
+    def authorized(self):
+        """
+        This is the route/function that the user will be redirected to by
+        the provider (e.g. Twitter) after the user has logged into the
+        provider's website and authorized your app to access their account.
+        """
+        if self.redirect_url:
+            next_url = self.redirect_url
+        elif self.redirect_to:
+            next_url = url_for(self.redirect_to)
+        else:
+            next_url = "/"
+        log.debug("next_url = %s", next_url)
+
+        # check for error in request args
+        error = request.args.get("error")
+        if error:
+            error_desc = request.args.get("error_description")
+            error_uri = request.args.get("error_uri")
+            log.warning(
+                "OAuth 2 authorization error: %s description: %s uri: %s",
+                error,
+                error_desc,
+                error_uri,
+            )
+            oauth_error.send(
+                self, error=error, error_description=error_desc, error_uri=error_uri
+            )
+            return redirect(next_url)
+
+        state_key = f"{self.name}_oauth_state"
+        if state_key not in flask.session:
+            # can't validate state, so redirect back to login view
+            log.info("state not found, redirecting user to login")
+            return redirect(url_for(".login"))
+
+        state = flask.session[state_key]
+        log.debug("state = %s", state)
+        self.session._state = state
+        del flask.session[state_key]
+
+        if self.redirect_uri:
+            self.session.redirect_uri = self.redirect_uri
+        else:
+            self.session.redirect_uri = url_for(".authorized", _external=True)
+
+        log.debug("client_id = %s", self.client_id)
+        log.debug("client_secret = %s", self.client_secret)
+        try:
+            token = self.session.fetch_token(
+                self.token_url,
+                authorization_response=request.url,
+                client_secret=self.client_secret,
+                **self.token_url_params,
+            )
+        except MissingCodeError as e:
+            e.args = (
+                e.args[0],
+                "The redirect request did not contain the expected parameters. Instead I got: {}".format(
+                    json.dumps(request.args)
+                ),
+            )
+            raise
+
+        results = oauth_authorized.send(self, token=token) or []
+        set_token = True
+        for func, ret in results:
+            if isinstance(ret, (Response, current_app.response_class)):
+                return ret
+            if ret == False:
+                set_token = False
+
+        if set_token:
+            try:
+                self.token = token
+            except ValueError as error:
+                log.warning("OAuth 2 authorization error: %s", str(error))
+                oauth_error.send(self, error=error)
+        return redirect(next_url)
