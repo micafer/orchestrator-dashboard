5f238f25f28fee97348fe098696b0f183ee41e28
Miguel Caballer <micafer1@upv.es>
2024-05-23 08:12:52 +0200

    Ser ver num 2.5.7

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@
 class Settings:
     def __init__(self, config):
         """Creator function."""
-        self.version = "2.5.6"
+        self.version = "2.5.7"
         self.toscaDir = config.get('TOSCA_TEMPLATES_DIR', '') + "/"
         self.imUrl = config['IM_URL']
         self.oidcName = config['OIDC_NAME']
