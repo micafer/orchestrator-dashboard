c35d24179c4a57c5fcfa96cbf72d6fce45cf2f8b
Miguel Caballer <micafer1@upv.es>
2024-10-28 10:38:59 +0100

    Maintain IM inf list order

--- app/__init__.py
+++ app/__init__.py
@@ -523,7 +523,7 @@ def create_app(oidc_blueprint=None):
                 infrastructures[inf_id]['cloud_type'] = infra_data["site"]["type"]
                 infrastructures[inf_id]['site'] = Markup(site_info)
 
-        return render_template('infrastructures.html', infrastructures=infrastructures, reload=reload_infid)
+        return render_template('infrastructures.html', infrastructures=infrastructures, reload=reload_infid, inf_list=inf_list)
 
     @app.route('/infrastructures/state')
     @authorized_with_valid_token
