dff9cb5c037a42817c93817824f648aa4fdb6bf9
Miguel Caballer <micafer1@upv.es>
2024-12-13 09:56:58 +0100

    Fix test

--- app/__init__.py
+++ app/__init__.py
@@ -247,7 +247,8 @@ def create_app(oidc_blueprint=None):
                 return render_template('home.html', oidc_name=settings.oidcName)
 
         # Force to get the user credentials to cache them
-        scheduler.add_job(func=utils.get_cache_creds, trigger='date', run_date=datetime.datetime.now(),
+        ndate = datetime.datetime.now() + datetime.timedelta(0,2)
+        scheduler.add_job(func=utils.get_cache_creds, trigger='date', run_date=ndate,
                           misfire_grace_time=20, args=[cred, get_cred_id()], id='get_cache_creds')
 
         # if there are any next url, redirect to it
